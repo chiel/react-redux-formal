64a21f8a5e8de9a8c1466871e1f3efe66e998732
Chiel Kunkels <chiel@kunkels.me>
2016-06-09 16:08:34 +0200

    index: remove unnecessary index in paths

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ export * as formActions from './formActions';
 export * as formConstants from './formConstants';
 export formReducer from './formReducer';
 
-export * as validators from './utils/validators/index';
+export * as validators from './utils/validators';
 
 export createDefaultFields from './utils/createDefaultFields';
 export createDefaultForm from './utils/createDefaultForm';
 export createDefaultValues from './utils/createDefaultValues';
 
-export * as inputTypes from './inputTypes/index';
+export * as inputTypes from './inputTypes';
